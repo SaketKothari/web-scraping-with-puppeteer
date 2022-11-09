70a651ea9f0b731bb5b52ba5932b8f5c6dae7639
Saket Kothari <saketkothari722@gmail.com>
2022-11-09 23:40:44 +0530

    Init Browser & Page Object

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Testing");
+const puppeteer = require('puppeteer');
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.traversy.media.com');
+
+  await browser.close();
+}
+
+run();
