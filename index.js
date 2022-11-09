ea8ebbb1d3960b0acb10b2bfdd157c1bef254d4c
Saket Kothari <saketkothari722@gmail.com>
2022-11-09 23:43:42 +0530

    🔨

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.traversy.media.com');
+  await page.goto('https://www.traversymedia.com');
 
   await browser.close();
 }
